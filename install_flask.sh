d5418bb6df0c764c3547a2e8f04392f266981dde
shevshinko <45826418+shevshinko@users.noreply.github.com>
2022-05-11 16:19:36 +0200

    Update install_flask.sh

--- install_flask.sh
+++ install_flask.sh
@@ -8,4 +8,19 @@ echo "Flask has been installed succesfully!!"
 echo "****************************************"
 
 line="@reboot sudo python3 /home/pi/SmartLock/run.py"
-(crontab -u $(whoami) -l; echo "$line" ) | crontab -u $(whoami) -
+(sudo crontab -l; echo "$line")
+
+read -p "Would you like to reboot now? (Y)-yes (N)-no " ASK
+
+while [ "$ASK" != "Y"||"y" ] && [ "$ASK" != "N"||"n" ]
+do
+	echo "No valid answer has been given... Try Again later!"
+done
+
+if [ "$ASK" = "Y"||"y" ]; then
+	#reboot
+	reboot
+
+else 
+  echo "Don't forget to reboot your RPi in order to get the programme working well! Thank you in advanced!"
+fi
